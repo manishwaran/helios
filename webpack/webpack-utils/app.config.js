9d436c7d76279afc3ee2b66e3e765ab04bd85786
manishwaran <manishwaran.np@gmail.com>
2017-05-21 09:11:51 +0530

    Scalfolding.

---
+++ webpack/webpack-utils/app.config.js
@@ -0,0 +1,100 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  bail: true,
+  entry: {
+    vendorbase: [
+      'react',
+      'react-dom',
+      'mobx',
+      'mobx-react',
+      'react-router',
+      'react-tap-event-plugin',
+      'material-ui',
+      'isomorphic-fetch',
+    ],
+    vendorAddon: [
+      'react-ace',
+      'react-addons-css-transition-group',
+      'react-keydown',
+      'brace',
+      'classnames',
+    ],
+    index: './app/index.jsx',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.scss'],
+  },
+  target: 'web',
+  output: {
+    path: 'dist/app',
+    filename: '[name].js',
+  },
+  module: {
+    loaders: [
+      {
+        enforce: 'pre',
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'eslint',
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
+        test: /\.s?css$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css', 'sass'],
+        }),
+      },
+      {
+        test: /\.(woff2?|svg)$/,
+        loader: 'url?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|gif|png)$/,
+        loader: 'file',
+      },
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Firestar-tagger-v2.0',
+      template: 'webpack/webpack-utils/template.ejs',
+      inject: true,
+      links: ['style.css'],
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      Tether: 'tether',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      'window.Tether': 'tether',
+    }),
+
+    new webpack.EnvironmentPlugin([
+      'NODE_ENV',
+    ]),
+
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      disable: false,
+      allChunks: true,
+    }),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'index',
+      chunks: ['vendorbase', 'vendorAddon', 'index'],
+    }),
+
+  ],
+};
---
+++ webpack/webpack.config.js
@@ -0,0 +1,4 @@
+const AppConfig = require('./webpack-utils/app.config');
+const ServerConfig = require('./webpack-utils/server.config');
+process.env.NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : "dev"
+module.exports = [ AppConfig, ServerConfig ];
